a1cb247b3097ff0151ab7ba22af569b607b6ff87
Joe Binns <joebinns.95@gmail.com>
2022-11-12 11:33:58 +0100

    Methodise word entry

--- Wordle/Assets/Scripts/TextEditor.cs
+++ Wordle/Assets/Scripts/TextEditor.cs
@@ -39,89 +39,123 @@ public class TextEditor : MonoBehaviour
         var tile = FindTileByCharacter(character);
         if (tile != null)
         {
-            if (_guessesBlockTilemapHandler.Tilemap.HasTile(_caretPosition))
-            {
-                // Set tile current
-                _guessesLetterTilemapHandler.Tilemap.SetTile(_caretPosition, tile);
-                _caretPosition.x++;
-            }
+            if (!_guessesBlockTilemapHandler.Tilemap.HasTile(_caretPosition)) { return; }
+            
+            // Set current tile
+            _guessesLetterTilemapHandler.Tilemap.SetTile(_caretPosition, tile);
+            _caretPosition.x++;
         }
         else
         {
-            if (_guessesBlockTilemapHandler.Tilemap.HasTile(_caretPosition + Vector3Int.left))
+            if (!_guessesBlockTilemapHandler.Tilemap.HasTile(_caretPosition + Vector3Int.left)) { return; }
+
+            // Delete previous tile
+            _caretPosition.x--;
+            _guessesLetterTilemapHandler.Tilemap.SetTile(_caretPosition, null);
+        }
+    }
+
+    private void SetTileStates(string word, ref Dictionary<int, TileState> indexToTileState)
+    {
+        for (int x = 0; x < word.Length; x++)
+        {
+            var character = word[x];
+            var tileState = TileState.WrongGuess;
+            if (_wordChecker.IsCharacterIncluded(character))
             {
-                // Delete previous tile
-                _caretPosition.x--;
-                _guessesLetterTilemapHandler.Tilemap.SetTile(_caretPosition, null);
+                tileState = TileState.SemiCorrectGuess;
+                if (_wordChecker.IsCharacterIncludedAtIndex(character, x))
+                {
+                    tileState = TileState.CorrectGuess;
+                }
+            }
+
+            indexToTileState[x] = tileState;
+        }
+    }
+
+    private void UndoExcessiveTileStates(string word, ref Dictionary<int, TileState> indexToTileState)
+    {
+        var charToExcess = _wordChecker.GetCharToExcess(word);
+        for (int x = word.Length - 1; x >= 0; x--)
+        {
+            var character = word[x];
+            if (indexToTileState[x] == TileState.SemiCorrectGuess)
+            {
+                if (charToExcess[character] > 0)
+                {
+                    indexToTileState[x] = TileState.WrongGuess;
+                    charToExcess[character] -= 1;
+                }
             }
         }
+    }
+
+    private void ApplyTileStates(string word, Dictionary<int, TileState> indexToTileState)
+    {
+        var position = new Vector3Int(0, _caretPosition.y, 0);
+        
+        for (int x = 0; x < word.Length; x++)
+        {
+            position.x = x;
+            var character = word[x];
+            var tileState = indexToTileState[x];
+            _guessesBlockTilemapHandler.SetTileState(position, tileState);
+            var keyboardPosition = _keyboardLetterTilemapTracker.TileNameToPosition(character.ToString());
+            _keyboardBlockTilemapHandler.SetTileStateCautious(keyboardPosition, tileState);
+        }
+    }
+
+    private bool CheckIfEntryIsValid()
+    {
+        var isWordValid = false;
+        var isLineFull = (!_guessesBlockTilemapHandler.Tilemap.HasTile(_caretPosition));
+        if (isLineFull)
+        {
+            var word = GetWord(_caretPosition.y);
+            var isWordRecognised = _wordChecker.IsWordRecognised(word);
+            if (isWordRecognised)
+            {
+                var isWordAlreadyEntered = _entries.Contains(word);
+                if (!isWordAlreadyEntered)
+                {
+                    isWordValid = true;
+                }
+            }
+        }
+        return isWordValid;
     }
 
     public void EnterText()
     {
         if (!_isEnabled) { return; }
 
-        if (!_guessesBlockTilemapHandler.Tilemap.HasTile(_caretPosition))
+        bool isEntryValid = CheckIfEntryIsValid();
+        if (!isEntryValid) { return;}
+
+        var word = GetWord(_caretPosition.y);
+        var indexToTileState = new Dictionary<int, TileState>();
+        
+        // Set tile states based on guess
+        SetTileStates(word, ref indexToTileState);
+
+        // Undo excessive tile states (i.e. in cases of duplicate characters)
+        UndoExcessiveTileStates(word, ref indexToTileState);
+
+        // Apply tiles based on tile states
+        ApplyTileStates(word, indexToTileState);
+
+        // Next line
+        NextRow();
+        _entries.Add(word);
+        
+        // Reset game if correct
+        var isWordCorrect = _wordChecker.DoesWordMatch(word);
+        if (isWordCorrect)
         {
-            var word = GetWord(_caretPosition.y);
-            if (_wordChecker.IsWordRecognised(word))
-            {
-                if (!_entries.Contains(word))
-                {
-                    var position = new Vector3Int(0, _caretPosition.y, 0);
-                    var indexToTileState = new Dictionary<int, TileState>();
-                    for (int x = 0; x < word.Length; x++)
-                    {
-                        var character = word[x];
-                        var tileState = TileState.WrongGuess;
-                        if (_wordChecker.IsCharacterIncluded(character))
-                        {
-                            tileState = TileState.SemiCorrectGuess;
-                            if (_wordChecker.IsCharacterIncludedAtIndex(character, x))
-                            {
-                                tileState = TileState.CorrectGuess;
-                            }
-                        }
-
-                        indexToTileState[x] = tileState;
-                    }
-
-                    var charToExcess = _wordChecker.GetCharToExcess(word);
-                    for (int x = word.Length - 1; x >= 0; x--)
-                    {
-                        var character = word[x];
-                        if (indexToTileState[x] == TileState.SemiCorrectGuess)
-                        {
-                            if (charToExcess[character] > 0)
-                            {
-                                indexToTileState[x] = TileState.WrongGuess;
-                                charToExcess[character] -= 1;
-                            }
-                        }
-                    }
-
-                    for (int x = 0; x < word.Length; x++)
-                    {
-                        position.x = x;
-                        var character = word[x];
-                        var tileState = indexToTileState[x];
-                        _guessesBlockTilemapHandler.SetTileState(position, tileState);
-                        var keyboardPosition = _keyboardLetterTilemapTracker.TileNameToPosition(character.ToString());
-                        _keyboardBlockTilemapHandler.SetTileStateCautious(keyboardPosition, tileState);
-                    }
-
-                    NextRow();
-                    _entries.Add(word);
-                    if (_wordChecker.DoesWordMatch(word))
-                    {
-                        _isEnabled = false;
-                        GameManager.Instance.ResetGame();
-                    }
-                }
-            }
+            TriggerReset();
         }
     }
-
 
     private void NextRow()
     {
@@ -129,8 +163,7 @@ public class TextEditor : MonoBehaviour
         _caretPosition.y--;
         if (!_guessesBlockTilemapHandler.Tilemap.HasTile(_caretPosition))
         {
-            _isEnabled = false;
-            GameManager.Instance.ResetGame();
+            TriggerReset();
         }
     }
     
@@ -144,6 +177,12 @@ public class TextEditor : MonoBehaviour
             word += _guessesLetterTilemapHandler.Tilemap.GetTile(coord).name;
         }
         return (word);
+    }
+
+    private void TriggerReset()
+    {
+        _isEnabled = false;
+        GameManager.Instance.ResetGame();
     }
 
     private void Reset()
